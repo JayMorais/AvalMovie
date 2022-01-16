c0b195e810d3e472ae8ee2a5e713ee4a5923155a
Jay Morais <jayannelcm@gmail.com>
2022-01-16 13:24:13 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/desenv_M/DesenvMApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desenv_M;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesenvMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesenvMApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/desenv_M/DesenvMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desenv_M;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenvMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
